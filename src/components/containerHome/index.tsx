4a933ab60452741d52520ba8a027f856b974f950
JupiterAgencia <JupiterAgencia@outlook.com.br>
2023-03-31 22:58:07 -0300

    first commit

--- src/components/containerHome/index.tsx
+++ src/components/containerHome/index.tsx
@@ -14,7 +14,9 @@ export function ContainerHome(){
                     Com anos de experiência no mercado, a Jupiter tem um forte compromisso em fornecer serviços de alta qualidade, que incluem design e desenvolvimento web, gerenciamento de conteúdo, otimização de mecanismos de pesquisa, hospedagem e suporte técnico.
                     </p>
 
-        
+                    <p>
+                    A empresa se destaca por seu foco em tecnologia, o que permite que seus clientes tenham acesso às soluções mais avançadas disponíveis no mercado. Além disso, a Jupiter está sempre atualizada com as últimas tendências e tecnologias, garantindo que seus clientes recebam um serviço atualizado e moderno.
+                    </p>
 
                     <p>
                     Se você está procurando uma empresa confiável e experiente para desenvolver seu blog, site ou e-commerce, a Jupiter é a escolha certa. Entre em contato conosco para saber mais sobre nossos serviços e como podemos ajudar a sua empresa a crescer online.
