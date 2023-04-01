20f20a4cdd71e3175fe3c3ca45d29ba3d5407fd2
JupiterAgencia <JupiterAgencia@outlook.com.br>
2023-04-01 13:40:59 -0300

    feat:add function menu mobile open and close

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,30 @@
 import "./style.scss";
 
 function Header(){
+
+    function handleClickClose() {
+        const menu = document.querySelector('.header-mobile')as HTMLElement;
+
+        menu.style.display = 'none';
+      }
+
+
+    function handleClickOpen() {
+        const menu = document.querySelector('.header-mobile')as HTMLElement;
+
+        menu.style.display = 'flex';
+      }
+    
     return (
         <div>
             <header>
                 <div className="logo">
                     <a href="#"><span>Jupiter Agência</span></a>
+                </div>
+                <div className="menu-btn-mobile" onClick={handleClickOpen}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" className="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
                 </div>
                 <ul>
                 <a href="#"><li>Início</li></a>
@@ -13,6 +32,24 @@ function Header(){
                 <a href="#"><li>Contato</li></a>
                 </ul>
             </header>
+            <div className="header-mobile">
+                    <div className="btn-close-header" onClick={handleClickClose}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                    </svg>
+                    </div>
+                    <ul>
+                        <a href="#">
+                            <li>Início</li>
+                        </a>
+                        <a href="#">
+                            <li>Sobre nós</li>
+                        </a>
+                        <a href="#">
+                            <li>Contato</li>
+                        </a>
+                    </ul>
+                </div>
         </div>
     )
 }
