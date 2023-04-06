0825e48c8e7d7db5a38947d956dd01f5a3c7dcea
JuanSantoss07 <juansantosscunha@gmail.com>
2023-04-06 00:00:18 -0300

    update links

--- src/components/containerHome/index.tsx
+++ src/components/containerHome/index.tsx
@@ -7,19 +7,22 @@ export function ContainerHome(){
     useEffect(() => {
         ScrollReveal().reveal('.containerHome_img', { origin:'right', distance:'100%', duration:2500 , mobile:false});
         ScrollReveal().reveal('.containerHome_text', { origin:'left', distance:'100%', duration:2500 , mobile:false});
-        typeWriter(document.querySelector('.teste') as HTMLElement);
+        typeWriter(document.querySelector('.maquina-escrever') as HTMLElement);
       }, []);
     
+      const linkContact = () => {
+        window.open('https://api.whatsapp.com/send?phone=13134707492&text=Ol%C3%A1%2C%20quero%20conhecer%20os%20planos%20da%20Jupiter%20Ag%C3%AAncia!')
+      }
     return (
         
         <>
             <section className="containerHome" id='home'>
                 <div className="containerHome_text">
                     <h1>Jupiter Agência</h1>
-                    <p className='teste'>
+                    <p className='maquina-escrever'>
                         O futuro é digital - e nós o construímos.
                     </p>
-                    <button className="btn-default btn-mobile">
+                    <button onClick={linkContact} className="btn-default btn-mobile">
                         Entre em contato
                     </button>
                 </div>
