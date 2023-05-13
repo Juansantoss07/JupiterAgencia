07b51d9161dc953f1169603f3a0bbff60935d95b
JuanSantoss07 <juansantosscunha@gmail.com>
2023-05-13 13:31:34 -0300

    fix: ajustes in our plans

--- src/components/ContainerOurPlans/index.tsx
+++ src/components/ContainerOurPlans/index.tsx
@@ -52,7 +52,7 @@ export default function ContainerOurPlans(){
 
                 <div className="container-ourplans_plan">
                     <h3>JupiConverto</h3>
-                    <video src="https://cdn.pixabay.com/vimeo/440059628/Reagir%20-%2044839.mp4?width=640&hash=0f1839b26ebd5c7dadfc215227ba703cbeacc5f9" autoPlay muted loop></video>
+                    <video src="https://cdn.pixabay.com/vimeo/440059628/Reagir%20-%2044839.mp4?width=640&hash=0f1839b26ebd5c7dadfc215227ba703cbeacc5f9w" autoPlay muted loop></video>
                     <p>Converto é uma plataforma de landing pages que permite aos usuários ter páginas de destino personalizadas para campanhas de marketing on-line, com modelos, testes A/B e integração com outras ferramentas.</p>
                     <button  className="btn-default btn-plan breve view-product-btn">Vizualizar Plataforma</button>
                     <button onClick={linkConverto} className="btn-default btn-plan breve">Em breve</button>
