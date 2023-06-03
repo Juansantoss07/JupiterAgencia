c2f93a248e35377606d183a892d3d9ceee41a2d1
JuanSantoss07 <juansantosscunha@gmail.com>
2023-06-03 12:35:16 -0300

    Updates in container our plans

--- src/components/ContainerOurPlans/index.tsx
+++ src/components/ContainerOurPlans/index.tsx
@@ -37,7 +37,7 @@ export default function ContainerOurPlans(){
                     <p>JupitWork é uma plataforma de portfolio que permite aos usuários criar um portfolio on-line personalizado, exibir e compartilhar facilmente seus trabalhos de forma elegante e profissional. 
                     </p>
                     <button onClick={linkPontoComPlataform} className="btn-default btn-plan view-product-btn">Vizualizar Plataforma</button>
-                    <button onClick={linkPontoCom} className="btn-default btn-plan">Quero esse</button>
+                    <button onClick={linkPontoCom} className="btn-default btn-plan btn-compre">Comprar por R$ 1.999,90 <br></br> <br></br>ou em até 6x com juros</button>
                 </div>
 
 
@@ -47,15 +47,16 @@ export default function ContainerOurPlans(){
                     <video src="/productX.mp4" muted autoPlay loop></video>
                     <p>JupiShop é uma plataforma de ecommerce que permite ter uma loja virtual personalizada com gerenciamento de produtos, pagamentos e rastreamento de pedidos.</p>
                     <button onClick={linkProductX_Plataform}  className="btn-default btn-plan view-product-btn">Vizualizar Plataforma</button>
-                    <button onClick={linkProductX} className="btn-default btn-plan">Quero esse</button>
+                    <button onClick={linkProductX} className="btn-default btn-plan btn-compre">Comprar por R$ 2.999,90 <br></br> <br />
+                    ou em até 6x com juros</button>
                 </div>
 
                 <div className="container-ourplans_plan">
                     <h3>JupiConverto</h3>
                     <video src="/breve.mp4" muted autoPlay loop></video>
                     <p>JupitConverto é uma plataforma de landing pages que permite aos usuários ter páginas de destino personalizadas para campanhas de marketing on-line, com modelos, testes A/B e integração com outras ferramentas.</p>
-                    <button  className="btn-default btn-plan breve view-product-btn">Vizualizar Plataforma</button>
-                    <button onClick={linkConverto} className="btn-default btn-plan breve">Em breve</button>
+                    <button  className="btn-default btn-plan view-product-btn">Vizualizar Plataforma</button>
+                    <button onClick={linkConverto} className="btn-default btn-plan breve">Plataforma ainda em <br /> <br /> desenvolvimento...</button>
                 </div>
                </div>
             </section>
