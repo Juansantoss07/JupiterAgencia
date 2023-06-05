cef836121cf9d0d0c3fa51eb7456a839a54e6185
JuanSantoss07 <juansantosscunha@gmail.com>
2023-06-05 17:43:22 -0300

    Updates in layouts colors theme

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -26,7 +26,7 @@ function Header(){
                     <a href="#home"><span>Jupiter Agência</span></a>
                 </div>
                 <div className="menu-btn-mobile" onClick={handleClickOpen}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" className="bi bi-list" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="black" className="bi bi-list" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
                         </svg>
                 </div>
